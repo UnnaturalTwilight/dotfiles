bdb8bdb254c8f6c4b96f528d401286c82a862b2c
UnnaturalTwilight <107954129+UnnaturalTwilight@users.noreply.github.com>
2026-03-06 23:16:35 -0500

    wsl fixes

--- zsh/wsl-arch.zsh
+++ zsh/wsl-arch.zsh
@@ -8,12 +8,18 @@ path=( ${path[@]:#*/games} )
 # add select windows apps to the path
 path=($path ~/.local/bin/windowsapps ~/.local/bin/winget)
 
+# Check if interop is working, since I rely on more than I should.
+# This is a far from foolproof check but it does catch the case that provides the least helpful error message
+cat /proc/sys/fs/binfmt_misc/WSLInterop &>/dev/null || echo "WSL interop is not working! Windows programs will not work" 
+
 # Kitty-specific aliases and functions, dependent shell integration // kittens
 # Making the assumption that if we're in kitty, the shell integration is loaded
 if [[ "$TERM" == "xterm-kitty" ]]; then
   source $ZDOTDIR/kitty.zsh
   bindkey '^[[99;6u' copy-buffer-to-clipboard
   bindkey '^[[120;6u' cut-buffer-to-clipboard
+elif [[ "$TERM_PROGRAM" == "vscode" ]]; then
+  source "$(code --locate-shell-integration-path zsh)"
 else
   # Alt-Shift-C
   bindkey '^[C' copy-buffer-to-clipboard
@@ -53,12 +59,12 @@ function run-win() {
 }
 
 function copy-buffer-to-clipboard() {
-  print -Rn "$BUFFER" | /mnt/c/Windows/System32/clip.exe
+  print -Rn "$BUFFER" | /mnt/c/Windows/System32/clip.exe 2>/dev/null
 }
 zle -N copy-buffer-to-clipboard
 
 function cut-buffer-to-clipboard() {
-  print -Rn "$BUFFER" | /mnt/c/Windows/System32/clip.exe
+  print -Rn "$BUFFER" | /mnt/c/Windows/System32/clip.exe 2>/dev/null
   zle kill-buffer
 }
 zle -N cut-buffer-to-clipboard
