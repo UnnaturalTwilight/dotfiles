1556134b68863aabcbf3cc4128331ae0f35c436b
UnnaturalTwilight <107954129+UnnaturalTwilight@users.noreply.github.com>
2026-09-14 09:37:44 -0400

    new setup bits

--- setup/gpg-systemd-setup.sh
+++ setup/gpg-systemd-setup.sh
@@ -1,107 +1,102 @@
 #! /usr/bin/bash
 # Script to produce the custom user socket files needed to not have ~/.gnugp
 
-if [ -z "$GNUPGHOME" ]; then
-  echo "\$GNUPGHOME is not set. Please set it to $HOME/.local/share/gnupg"
+export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:="$HOME/.config"}
+
+if [ -z "${GNUPGHOME}" ]; then
+  echo "\$GNUPGHOME is not set. Please set it to ${XDG_CONFIG_HOME}/gnupg"
   exit 1
 fi
 
 echo "Setting up GPG systemd user socket files..."
-echo "GNUPGHOME: $GNUPGHOME"
-mkdir -m 700 "$GNUPGHOME"
+echo "GNUPGHOME: ${GNUPGHOME}"
+mkdir -m 700 "${GNUPGHOME}"
 
 socketdir=$(gpgconf --list-dirs socketdir | sed s/'\/run\/user\/.*\/gnupg'/'%t\/gnupg'/)
-echo "Socket directory: $socketdir"
-mkdir -p $HOME/.config/systemd/user/gpg-agent.service.d
+echo "Socket directory: ${socketdir}"
+mkdir -p "${XDG_CONFIG_HOME}/systemd/user/gpg-agent.service.d"
 
-mv $HOME/.config/systemd/user/gpg-agent.service.d/override.conf $HOME/.config/systemd/user/gpg-agent.service.d/override.conf.bak 2>/dev/null
-cat << EOF > $HOME/.config/systemd/user/gpg-agent.service.d/override.conf
-
+mv "${XDG_CONFIG_HOME}/systemd/user/gpg-agent.service.d/override.conf" "${XDG_CONFIG_HOME}/systemd/user/gpg-agent.service.d/override.conf.bak" 2>/dev/null
+cat << EOF > "${XDG_CONFIG_HOME}/systemd/user/gpg-agent.service.d/override.conf"
 [Service]
 Environment="GNUPGHOME=%h/.local/share/gnupg"
 EOF
 
-mv $HOME/.config/systemd/user/gpg-agent.socket $HOME/.config/systemd/user/gpg-agent.socket.bak 2>/dev/null
-cat << EOF > $HOME/.config/systemd/user/gpg-agent.socket
-
+mv "${XDG_CONFIG_HOME}/systemd/user/gpg-agent.socket" "${XDG_CONFIG_HOME}/systemd/user/gpg-agent.socket.bak" 2>/dev/null
+cat << EOF > "${XDG_CONFIG_HOME}/systemd/user/gpg-agent.socket"
 [Unit]
 Description=GnuPG cryptographic agent and passphrase cache
 Documentation=man:gpg-agent(1)
 
 [Socket]
-ListenStream=$socketdir/S.gpg-agent
+ListenStream=${socketdir}/S.gpg-agent
 FileDescriptorName=std
 SocketMode=0600
 DirectoryMode=0700
 EOF
 
-mv $HOME/.config/systemd/user/gpg-agent-ssh.socket $HOME/.config/systemd/user/gpg-agent-ssh.socket.bak 2>/dev/null
-cat << EOF > $HOME/.config/systemd/user/gpg-agent-ssh.socket
-
+mv "${XDG_CONFIG_HOME}/systemd/user/gpg-agent-ssh.socket" "${XDG_CONFIG_HOME}/systemd/user/gpg-agent-ssh.socket.bak" 2>/dev/null
+cat << EOF > "${XDG_CONFIG_HOME}/systemd/user/gpg-agent-ssh.socket"
 [Unit]
 Description=GnuPG cryptographic agent (ssh-agent emulation)
 Documentation=man:gpg-agent(1) man:ssh-add(1) man:ssh-agent(1) man:ssh(1)
 
 [Socket]
-ListenStream=$socketdir/S.gpg-agent.ssh
+ListenStream=${socketdir}/S.gpg-agent.ssh
 FileDescriptorName=ssh
 Service=gpg-agent.service
 SocketMode=0600
 DirectoryMode=0700
 EOF
 
-mv $HOME/.config/systemd/user/gpg-agent-extra.socket $HOME/.config/systemd/user/gpg-agent-extra.socket.bak 2>/dev/null
-cat << EOF > $HOME/.config/systemd/user/gpg-agent-extra.socket
-
+mv "${XDG_CONFIG_HOME}/systemd/user/gpg-agent-extra.socket" "${XDG_CONFIG_HOME}/systemd/user/gpg-agent-extra.socket.bak" 2>/dev/null
+cat << EOF > "${XDG_CONFIG_HOME}/systemd/user/gpg-agent-extra.socket"
 [Unit]
 Description=GnuPG cryptographic agent and passphrase cache (restricted)
 Documentation=man:gpg-agent(1)
 
 [Socket]
-ListenStream=$socketdir/S.gpg-agent.browser
+ListenStream=${socketdir}/S.gpg-agent.browser
 FileDescriptorName=extra
 Service=gpg-agent.service
 SocketMode=0600
 DirectoryMode=0700
 EOF
 
-mv $HOME/.config/systemd/user/gpg-agent-browser.socket $HOME/.config/systemd/user/gpg-agent-browser.socket.bak 2>/dev/null
-cat << EOF > $HOME/.config/systemd/user/gpg-agent-browser.socket
-
+mv "${XDG_CONFIG_HOME}/systemd/user/gpg-agent-browser.socket" "${XDG_CONFIG_HOME}/systemd/user/gpg-agent-browser.socket.bak" 2>/dev/null
+cat << EOF > "${XDG_CONFIG_HOME}/systemd/user/gpg-agent-browser.socket"
 [Unit]
 Description=GnuPG cryptographic agent and passphrase cache (access for web browsers)
 Documentation=man:gpg-agent(1)
 
 [Socket]
-ListenStream=$socketdir/S.gpg-agent.browser
+ListenStream=${socketdir}/S.gpg-agent.browser
 FileDescriptorName=browser
 Service=gpg-agent.service
 SocketMode=0600
 DirectoryMode=0700
 EOF
 
-mv $HOME/.config/systemd/user/dirmngr.socket $HOME/.config/systemd/user/dirmngr.socket.bak 2>/dev/null
-cat << EOF > $HOME/.config/systemd/user/dirmngr.socket
-
+mv "${XDG_CONFIG_HOME}/systemd/user/dirmngr.socket" "${XDG_CONFIG_HOME}/systemd/user/dirmngr.socket.bak" 2>/dev/null
+cat << EOF > "${XDG_CONFIG_HOME}/systemd/user/dirmngr.socket"
 [Unit]
 Description=GnuPG network certificate management daemon
 Documentation=man:dirmngr(8)
 
 [Socket]
-ListenStream=$socketdir/S.dirmngr
+ListenStream=${socketdir}/S.dirmngr
 SocketMode=0600
 DirectoryMode=0700
 EOF
 
-mv $HOME/.config/systemd/user/keyboxd.socket $HOME/.config/systemd/user/keyboxd.socket.bak 2>/dev/null
-cat << EOF > $HOME/.config/systemd/user/keyboxd.socket
-
+mv "${XDG_CONFIG_HOME}/systemd/user/keyboxd.socket" "${XDG_CONFIG_HOME}/systemd/user/keyboxd.socket.bak" 2>/dev/null
+cat << EOF > "${XDG_CONFIG_HOME}/systemd/user/keyboxd.socket"
 [Unit]
 Description=GnuPG public key management service
 Documentation=man:keyboxd(8)
 
 [Socket]
-ListenStream=$socketdir/S.keyboxd
+ListenStream=${socketdir}/S.keyboxd
 FileDescriptorName=std
 SocketMode=0600
 DirectoryMode=0700
--- setup/simlinks.zsh
+++ setup/simlinks.zsh
@@ -2,7 +2,7 @@
 # Setup the simlinks between .config and this repo
 builtin cd ~/dotfiles || exit 1
 
-if [ -z "$ZDOTDIR" ]; then
+if [ -z "${ZDOTDIR}" ]; then
   # ZDOTDIR is the only real thing we can check for this
   echo "\$ZDOTDIR is not set. Make sure to set up the system wide config first."
   exit 1
@@ -15,46 +15,51 @@ host=${HOST:-$(cat /etc/hostname)}
 user=${USER:-$USERNAME}
 
 ## Create symlinks for bash
-ln -sfbr --suffix=.bak --target-directory=$HOME ./bash/.bashrc
-ln -sfbr --suffix=.bak --target-directory=$HOME ./bash/.bash_profile
+ln -sfbr --suffix=.bak --target-directory="${HOME}" ./bash/.bashrc
+ln -sfbr --suffix=.bak --target-directory="${HOME}" ./bash/.bash_profile
 
 ## Create symlinks for zsh
-mkdir -p $ZDOTDIR
-ln -sfbr --suffix=.bak --target-directory=$ZDOTDIR ./zsh/.*
-ln -sfbr --suffix=.bak --target-directory=$ZDOTDIR ./zsh/keybinds.zsh
-ln -sfbr --suffix=.bak --target-directory=$ZDOTDIR ./zsh/kitty.zsh
-ln -sfbr --suffix=.bak --target-directory=$ZDOTDIR ./zsh/$host.zsh
+mkdir -p "${ZDOTDIR}"
+ln -sfbr --suffix=.bak --target-directory="${ZDOTDIR}" ./zsh/.*
+ln -sfbr --suffix=.bak --target-directory="${ZDOTDIR}" ./zsh/keybinds.zsh
+ln -sfbr --suffix=.bak --target-directory="${ZDOTDIR}" ./zsh/kitty.zsh
+ln -sfbr --suffix=.bak --target-directory="${ZDOTDIR}" ./zsh/${host}.zsh
 
 # Pull in the XDG vars
-source $ZDOTDIR/.zshenv
+source "${ZDOTDIR}/.zshenv"
 
 # Make sure zsh reads $ZDOTDIR/.zshrc
-mv $HOME/.zshrc $HOME/.zshrc.bak 2>/dev/null
+mv "${HOME}/.zshrc" "${HOME}/.zshrc.bak" 2>/dev/null
 
 # make a couple dirs that just need to exist
-mkdir -p $XDG_STATE_HOME/bash
-mkdir -p $XDG_STATE_HOME/zsh
-mkdir -p $XDG_DATA_HOME/applications
-mkdir -p $HOME/.local/bin
+mkdir -p "${XDG_STATE_HOME}/bash"
+mkdir -p "${XDG_STATE_HOME}/zsh"
+mkdir -p "${XDG_DATA_HOME}/applications"
+mkdir -p "${HOME}/.local/bin"
+mkdir -p "${XDG_CONFIG_HOME}/environment.d"
+mkdir --mode=700 "${XDG_CONFIG_HOME}/credstore.encrypted"
+
+# environment.d contents
+ln -sfbr --suffix=.bak --target-directory="${XDG_CONFIG_HOME}/environment.d" ./environment.d/*.conf
 
 # Simple Links
-ln -sfbr --suffix=.bak --target-directory=$XDG_CONFIG_HOME ./starship.toml
-ln -sfbr --suffix=.bak --target-directory=$XDG_CONFIG_HOME ./hyfetch.json
-ln -sfbr --suffix=.bak --target-directory=$XDG_CONFIG_HOME ./fastfetch
-ln -sfbr --suffix=.bak --target-directory=$XDG_CONFIG_HOME ./kitty
-ln -sfbr --suffix=.bak --target-directory=$XDG_CONFIG_HOME ./msedit
-ln -sfbr --suffix=.bak --target-directory=$XDG_CONFIG_HOME ./fzf
-ln -sfbr --suffix=.bak --target-directory=$XDG_CONFIG_HOME ./xdg-desktop-portal
-ln -sfbr --suffix=.bak --target-directory=$XDG_CONFIG_HOME ./zed
+ln -sfbr --suffix=.bak --target-directory="${XDG_CONFIG_HOME}" ./starship.toml
+ln -sfbr --suffix=.bak --target-directory="${XDG_CONFIG_HOME}" ./hyfetch.json
+ln -sfbr --suffix=.bak --target-directory="${XDG_CONFIG_HOME}" ./fastfetch
+ln -sfbr --suffix=.bak --target-directory="${XDG_CONFIG_HOME}" ./kitty
+ln -sfbr --suffix=.bak --target-directory="${XDG_CONFIG_HOME}" ./msedit
+ln -sfbr --suffix=.bak --target-directory="${XDG_CONFIG_HOME}" ./fzf
+ln -sfbr --suffix=.bak --target-directory="${XDG_CONFIG_HOME}" ./xdg-desktop-portal
+ln -sfbr --suffix=.bak --target-directory="${XDG_CONFIG_HOME}" ./zed
 
 # Assets (images, etc.)
-ln -sfbr --suffix=.bak ./assets $XDG_CONFIG_HOME/assets
-ln -sfbr --suffix=.bak $XDG_CONFIG_HOME/assets/${user}_profilepic.png $XDG_CONFIG_HOME/profilepic.png
+ln -sfbr --suffix=.bak ./assets "${XDG_CONFIG_HOME}/assets"
+ln -sfbr --suffix=.bak "${XDG_CONFIG_HOME}/assets/${user}_profilepic.png" "${XDG_CONFIG_HOME}/profilepic.png"
 
 # Yazi
-mkdir $XDG_CONFIG_HOME/yazi
-ln -sfbr --suffix=.bak --target-directory=$XDG_CONFIG_HOME/yazi ./yazi/*
-cp -fLT --remove-destination ./yazi/package.toml $XDG_CONFIG_HOME/yazi/package.toml
+mkdir "${XDG_CONFIG_HOME}/yazi"
+ln -sfbr --suffix=.bak --target-directory="${XDG_CONFIG_HOME}/yazi" ./yazi/*
+cp -fLT --remove-destination ./yazi/package.toml "${XDG_CONFIG_HOME}/yazi/package.toml"
 if (command -pv ya); then
   echo
   ya pkg install --discard
@@ -66,11 +71,11 @@ else
 fi
 
 # Desktop files
-ln -sfbr --suffix=.bak --target-directory=$XDG_DATA_HOME/applications ./desktop/unicode-picker.desktop
-ln -sfbr --suffix=.bak --target-directory=$XDG_DATA_HOME/applications ./desktop/vlc.desktop
+ln -sfbr --suffix=.bak --target-directory="${XDG_DATA_HOME}/applications" ./desktop/unicode-picker.desktop
+ln -sfbr --suffix=.bak --target-directory="${XDG_DATA_HOME}/applications" ./desktop/vlc.desktop
 
 # Scripts
-ln -sfbr --suffix=.bak --target-directory=$HOME/.local/bin ./scripts/unicode-panel.sh
+ln -sfbr --suffix=.bak --target-directory="${HOME}/.local/bin" ./scripts/unicode-panel.sh
 
 echo
 echo "THIS SCRIPT IS WIP"
@@ -79,6 +84,6 @@ echo "All other links need to be created by hand"
 # SDDM
 echo
 echo "Manualy add profile pic to /usr/share/sddm/faces as ${user}.face.icon to get it to show up in SDDM"
-echo "sudo ln -s $XDG_CONFIG_HOME/profilepic.png /usr/share/sddm/faces/${user}.face.icon"
+echo "sudo ln -s \$XDG_CONFIG_HOME/profilepic.png /usr/share/sddm/faces/${user}.face.icon"
 echo "sudo setfacl -m u:sddm:r /usr/share/sddm/faces/${user}.face.icon"
 echo
